4d53b665e494bd9d3adccb4b624534bb8cb71d82
sachin01k <sachinkamble825@gmail.com>
2023-12-10 13:38:33 +0530

    Adding n numbers and Returning sum Using Rescursion

---
+++ SumRecursion.java
@@ -0,0 +1,16 @@
+
+public class SumRecursion{
+
+	public static int sum(int n)
+	{
+		if(n == 1)
+			return n;
+
+		return n + sum(n-1);		
+	}
+
+	public static void main(String []args)
+	{
+		System.out.println("Sum : " + sum(5));
+	}
+}
